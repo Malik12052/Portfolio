032efd2ce8f5a48987d888f9a3f932d4360a5252
Gabriel Felix <Gabe12052@gmail.com>
2024-08-23 08:47:49 -0600

    added galaxy fixed png and name

--- components/Bio.js
+++ components/Bio.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { motion, useMotionTemplate, useMotionValue, animate, useSpring, useTransform } from "framer-motion";
+import { motion, useMotionTemplate, useMotionValue, animate } from "framer-motion";
 import { twMerge } from "tailwind-merge";
 import { FiArrowRight, FiMail, FiMapPin } from "react-icons/fi";
 import { SiGithub, SiTwitter, SiSoundcloud, SiInstagram } from "react-icons/si";
@@ -33,7 +33,7 @@ export const Bio = () => {
         backgroundImage,
       }}
     >
-    
+      
       <motion.div
         initial="initial"
         animate="animate"
@@ -58,7 +58,6 @@ export const Bio = () => {
     </div>
   );
 };
-
 const Block = ({ className, border, boxShadow, ...rest }) => {
   return (
     <motion.div
@@ -102,6 +101,7 @@ const HeaderBlock = ({ textGradient }) => (
     />
     <h1
       className="mb-12 text-4xl font-medium leading-tight"
+      
     >
       Hi, I'm Gabriel.{" "}
       <span className="text-white">
@@ -117,118 +117,78 @@ const HeaderBlock = ({ textGradient }) => (
   </Block>
 );
 
-const SocialsBlock = () => {
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-  const rotateX = useTransform(y, [-100, 100], [10, -10]);
-  const rotateY = useTransform(x, [-100, 100], [-10, 10]);
-  const springConfig = { stiffness: 400, damping: 30 };
-  const scale = useSpring(1, springConfig);
-
-  const handleMouseMove = (e) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    x.set(e.clientX - rect.left - rect.width / 2);
-    y.set(e.clientY - rect.top - rect.height / 2);
-  };
-
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
-    scale.set(1);
-  };
-
-  return (
-    <motion.div
-      className="col-span-12 md:col-span-6 p-4 rounded-lg border-2 border-transparent"
-      style={{
-        background: 'linear-gradient(135deg, rgba(19,255,170,0.5), rgba(221,51,92,0.5))',
-        borderImageSlice: 1,
-        borderWidth: '2px',
-        borderImageSource: 'linear-gradient(135deg, #13FFAA, #DD335C)',
-        boxShadow: '0px 4px 24px rgba(19,255,170,0.5)',
-        perspective: 1000,
+const SocialsBlock = () => (
+  <>
+    <Block
+      whileHover={{
+        rotate: "2.5deg",
+        scale: 1.1,
       }}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      animate={{ scale }}
+      className="col-span-6 bg-orange-500 md:col-span-3"
     >
-      <motion.div style={{ rotateX, rotateY }}>
-        <Block
-          whileHover={{
-            rotate: "2.5deg",
-            scale: 1.2,
-            backgroundColor: "rgba(255, 140, 0, 0.8)",
-          }}
-          className="col-span-6 bg-orange-500 md:col-span-3"
-        >
-          <a
-            href="https://soundcloud.com/feelx1"
-            className="grid h-full place-content-center text-3xl text-white"
-          >
-            <SiSoundcloud />
-          </a>
-        </Block>
-        <Block
-          whileHover={{
-            rotate: "-2.5deg",
-            scale: 1.2,
-            backgroundColor: "rgba(0, 128, 0, 0.8)",
-          }}
-          className="col-span-6 bg-green-600 md:col-span-3"
-        >
-          <a
-            href="https://github.com/Malik12052"
-            className="grid h-full place-content-center text-3xl text-white"
-          >
-            <SiGithub />
-          </a>
-        </Block>
-        <Block
-          whileHover={{
-            rotate: "-2.5deg",
-            scale: 1.2,
-            backgroundColor: "rgba(255, 20, 147, 0.8)",
-          }}
-          className="col-span-6 bg-pink-500 md:col-span-3"
-        >
-          <a
-            href="https://www.instagram.com/_feelx_/?igsh=MTFkeTFzN243bGxmaQ%3D%3D&utm_source=qr&fbclid=IwY2xjawEq0BpleHRuA2FlbQIxMAABHQRM8-635f_bofWglAf_rKlpsTcdTpFdFsSkG1WYSQO8T1NDSCj2uJcIZA_aem_FDBNISvNJ0bTKeTs_nwTgg"
-            className="grid h-full place-content-center text-3xl text-white"
-          >
-            <SiInstagram />
-          </a>
-        </Block>
-        <Block
-          whileHover={{
-            rotate: "2.5deg",
-            scale: 1.2,
-            backgroundColor: "rgba(30, 144, 255, 0.8)",
-          }}
-          className="col-span-6 bg-blue-500 md:col-span-3"
-        >
-          <a
-            href="https://x.com/Bearsace12052"
-            className="grid h-full place-content-center text-3xl text-white"
-          >
-            <SiTwitter />
-          </a>
-        </Block>
-      </motion.div>
-    </motion.div>
-  );
-};
+      <a
+        href="YOUR_SOUNDCLOUD_URL" // Replace with your SoundCloud profile link
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiSoundcloud />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-green-600 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiGithub />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-zinc-50 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-black"
+      >
+        <SiInstagram />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-blue-500 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiTwitter />
+      </a>
+    </Block>
+  </>
+);
 
 const AboutBlock = () => (
   <Block className="col-span-12 text-3xl leading-snug">
     <p>
-      
-      <span className="text-zinc-400">
-      My passion is to help bring peoples dreams to reality. I build with Next.js, Javascript, React, Tailwind CSS, Solidity for blockchain and Framer Motion. Another things i do is make Jewlery. I create music using Ableton and the top of the line plugins. I also have skills in graphic designs. Connect with me and lets make your dreams come true! 
-</span>
-          </p>
+      My passion is building cool stuff.{" "}
+      {/* <span className="text-zinc-400"> */}
+      My passion is to help bring peoples dreams to reality. I build with Next.js, Javascript, React, Tailwind CSS, Solidity for blockchain and Framer Motion. Another things i do is make  I create music using Ableton and the top of the line plugins. I also have skills creating graphic designs. Connect with me and lets make your dreams come true!         I build primarily with React, Tailwind CSS, and Framer Motion. I love
+        this stack so much that I even built a website about it. I've made over
+        a hundred videos on the subject across YouTube and TikTok.
+    </p>
   </Block>
 );
-
 
 const LocationBlock = () => (
   <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
@@ -287,7 +247,7 @@ const Footer = () => {
       <p className="text-center text-zinc-400">
         Made with ❤️ by{" "}
         <a href="#" className="text-red-300 hover:underline">
-          Your's Truly
+          @tomisloading
         </a>
       </p>
     </footer>
--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,15 @@
 import { Stars } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import Image from 'next/image';
-import React, { useEffect, Suspense } from "react";
+import React, { useEffect } from "react";
 import { FiArrowRight } from "react-icons/fi";
-import { useMotionTemplate, useMotionValue, motion, animate } from "framer-motion";
-import Particles from "react-tsparticles";
+import 'animate.css';
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate,
+} from "framer-motion";
+import * as THREE from 'three';
 
 const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
 
@@ -25,107 +30,109 @@ export const AuroraHero = () => {
   const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
   const textGradient = useMotionTemplate`linear-gradient(45deg, ${color}, #fff)`;
 
-  // Function to handle mouse movement
-  const handleMouseMove = (event) => {
-    const { clientX, clientY } = event;
-    const x = (clientX / window.innerWidth) * 2 - 1;
-    const y = (clientY / window.innerHeight) * 2 - 1;
-    color.set(`rgb(${Math.abs(x) * 255}, ${Math.abs(y) * 255}, 255)`);
-  };
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => window.removeEventListener('mousemove', handleMouseMove);
-  }, []);
-
   return (
     <motion.section
-      style={{ backgroundImage }}
-      className="relative min-h-screen overflow-hidden bg-gray-950 px-4 py-24 text-gray-200 flex flex-col justify-center items-center"
+      style={{
+        backgroundImage,
+      }}
+      className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
     >
+      <motion.div
+        className="flex flex-col items-center justify-center"
+        style={{
+          position: "absolute",
+          top: "5%",
+          left: "39%",
+          transform: "translate(-50%, -50%)",
+        }}
+        animate={{
+          y: [0, -10, 0], // Vertical floating effect
+          scale: [1, 1.05, 1], // Subtle scaling effect
+        }}
+        transition={{
+          duration: 4,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "mirror",
+        }}
+      >
+        <img
+          src="/papi.png"
+          alt="Gabe Felix"
+          className="w-48 h-48 rounded-full object-cover shadow-lg md:w-64 md:h-64"
+        />
+      </motion.div>
+
+      <motion.h1
+  className="text-center text-2xl font-medium leading-tight sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight matemasie-regular mt-48" // Changed mt-4 to mt-24
+  style={{
+    backgroundImage: textGradient,
+    backgroundClip: 'text',
+    color: 'transparent',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  }}
+  animate={{
+    scale: [1.5, 0.55, 1], // Subtle scaling effect
+  }}
+  transition={{
+    duration: 4,
+    ease: "easeInOut",
+    repeat: Infinity,
+    repeatType: "mirror",
+  }}
+>
+  Gabriel Felix
+</motion.h1>
+
+
+      <motion.p
+        className="my-6 max-w-xxl text-center text-2xl leading-relaxed md:text-4xl md:leading-relaxed matemasie-regular"
+        style={{
+          backgroundImage: textGradient, // Use the animated gradient
+          backgroundClip: 'text',
+          color: 'transparent',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          textShadow: '0px 4px 24px rgba(0, 0, 0, 0.5)', // Subtle text shadow
+        }}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5, duration: 1 }}
+        whileHover={{
+          scale: 1.05, // Slight scaling on hover
+          textShadow: '0px 4px 24px rgba(255, 255, 255, 0.8)', // Glow effect on hover
+        }}
+      >
+        Entrepreneur, Musician, Artist, Software Engineer, Blockchain Developer.
+        I create, innovate, and explore the possibilities at the intersection of art, technology, and business.
+      </motion.p>
+
+      <motion.button
+        style={{ 
+          border,
+          boxShadow,
+        }}
+        whileHover={{
+          scale: 1.015,
+        }}
+        whileTap={{
+          scale: 0.985,
+        }}
+        className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50 matemasie-regular mt-1"
+      >
+        Explore My Portfolio
+        <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+      </motion.button>
+
       <div className="absolute inset-0 z-0">
         <Canvas>
-          <Stars radius={150} count={2000} factor={25} fade speed={1} />
-          <Stars radius={200} count={3000} factor={35} fade speed={0.8} />
-          <Stars radius={250} count={4000} factor={45} fade speed={0.5} />
-          {/* Adding a 3D model */}
-          <Suspense fallback={null}>
-            {/* Add your 3D model here */}
-          </Suspense>
+          <Stars radius={350} count={1000} factor={70} fade speed={1} />
+          <points position={[0, -5.25, 0]}>
+            <sphereGeometry args={[2, 35, 32]} />
+            <pointsMaterial color="#ffffff" size={0.05} />
+          </points>
         </Canvas>
-        <Particles
-          options={{
-            particles: {
-              number: { value: 50 },
-              size: { value: 3 },
-              move: { speed: 1, direction: "top" },
-              interactivity: {
-                events: {
-                  onHover: { enable: true, mode: "repulse" },
-                },
-              },
-            },
-          }}
-        />
-      </div>
-
-      <div className="flex flex-col items-center text-center space-y-8 relative z-10">
-        <motion.div
-          animate={{ y: [0, -10, 0], scale: [1, 1.05, 1] }}
-          transition={{ duration: 4, ease: "easeInOut", repeat: Infinity, repeatType: "mirror" }}
-          className="relative z-10 bg-gray-950 rounded-full p-2"
-        >
-          <Image
-            src="/papi.png"
-            alt="Portrait of Gabriel Felix"
-            width={256}
-            height={256}
-            className="rounded-full object-cover shadow-lg"
-          />
-        </motion.div>
-
-        <motion.h1
-          initial={{ y: 100, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1 }}
-          viewport={{ once: true }}
-          className="text-center text-3xl font-medium leading-tight sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight matemasie-regular"
-          style={{
-            backgroundImage: textGradient,
-            WebkitBackgroundClip: 'text',
-            color: 'transparent',
-          }}
-        >
-          Gabriel Felix
-        </motion.h1>
-
-        <motion.p
-          className="max-w-xxl text-center text-2xl leading-10 sm:text-xl sm:leading-relaxed matemasie-regular"
-          style={{
-            backgroundImage: textGradient,
-            WebkitBackgroundClip: 'text',
-            color: 'transparent',
-          }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 1 }}
-        >
-          Entrepreneur, Musician, Artist, Software Engineer, Blockchain Developer.
-          I create, innovate, and explore the possibilities at the intersection of art, technology, and business.
-        </motion.p>
-
-        <motion.button
-          style={{ border, boxShadow }}
-          whileHover={{ scale: 1.015, boxShadow: "0px 0px 20px 5px rgba(255, 255, 255, 0.5)" }}
-          animate={{
-            scale: [1, 1.05, 1],
-            transition: { duration: 2, yoyo: Infinity }
-          }}
-          className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50 matemasie-regular"
-        >
-          Explore My Portfolio
-          <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
-        </motion.button>
       </div>
     </motion.section>
   );
